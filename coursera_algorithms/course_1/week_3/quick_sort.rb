f4ca067804a9d28d0ffd160f9e56fdd6aac28f9f
Argonus <argonuspiotr@gmail.com>
2017-03-16 20:45:42 +0100

    Implement full logic to quick sort, refactor specs.

--- coursera_algorithms/course_1/week_3/quick_sort.rb
+++ coursera_algorithms/course_1/week_3/quick_sort.rb
@@ -3,10 +3,44 @@ module Course1
     class QuickSort
       def initialize(array)
         @array = array
+        @a_length = array.size
       end
 
       def sort!
-        return @array if @array.empty?
+        return @array if @a_length <= 1
+
+        quicksort(@array, 0, @a_length)
+      end
+
+      private
+
+      def quicksort(array, left, right)
+        return array if left >= right
+
+        current = partition(array,left,right)
+        quicksort(array, left, current)
+        quicksort(array, current+1, right)
+        array
+      end
+
+      def partition(array, left, right)
+        pivot, i = array[left], left+1
+
+        (i...right).each do |j|
+          i = swap_and_increment(array, i, j) if array[j] < pivot
+        end
+
+        swap_arrays(array, i-1, left)
+        i-1
+      end
+
+      def swap_and_increment(array, i, j)
+        swap_arrays(array, i, j)
+        i + 1
+      end
+
+      def swap_arrays(array, i, j)
+        array[j], array[i] = array[i], array[j]
       end
     end
   end
--- coursera_algorithms/spec/course_1/week_3/quick_sort_spec.rb
+++ coursera_algorithms/spec/course_1/week_3/quick_sort_spec.rb
@@ -2,11 +2,21 @@ require_relative '../../spec_helper'
 require_relative '../../../course_1/week_3/quick_sort'
 
 RSpec.describe Course1::Week3::QuickSort do
-  [
-    []
-  ].each do |array|
-    it 'sorts given array' do
+
+  it "returns empty array when empty array is given" do
+    expect(described_class.new([]).sort!).to be_empty
+  end
+
+  (0..100).to_a.map do |range|
+    (0..range).to_a.shuffle
+  end.each do |array|
+    it "for #{array} it sorts its properly" do
       expect(described_class.new(array).sort!).to eq array.sort
     end
+  end
+
+  it "is not creating another array" do
+    array = [1, 2, 3]
+    expect(described_class.new(array).sort!).to equal array
   end
 end
