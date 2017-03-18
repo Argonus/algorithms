66404eb8f87c29cafa92b5f4925ae84a44c9b6eb
Argonus <argonuspiotr@gmail.com>
2017-03-18 20:53:04 +0100

    Refactor code to work with coursera programming task.
    Add availability to select manner which pivot will be selected.
    Add comparasions counting

--- coursera_algorithms/course_1/week_3/quick_sort.rb
+++ coursera_algorithms/course_1/week_3/quick_sort.rb
@@ -1,9 +1,13 @@
 module Course1
   module Week3
     class QuickSort
-      def initialize(array)
+      attr_reader :comparasions
+
+      def initialize(array, manner = :random)
         @array = array
         @a_length = array.size
+        @comparasions = 0
+        @manner = manner
       end
 
       def sort!
@@ -17,14 +21,15 @@ module Course1
       def quicksort(array, left, right)
         return array if left >= right
 
-        current = partition(array,left,right)
+        current = partition(array, left, right)
         quicksort(array, left, current)
         quicksort(array, current+1, right)
         array
       end
 
       def partition(array, left, right)
-        pivot, i = array[left], left+1
+        pivot, i = select_pivot_and_i(array, left, right)
+        increment_number_of_comparisons(left, right)
 
         (i...right).each do |j|
           i = swap_and_increment(array, i, j) if array[j] < pivot
@@ -34,6 +39,10 @@ module Course1
         i-1
       end
 
+      def increment_number_of_comparisons(left, right)
+        @comparasions += right-left-1
+      end
+
       def swap_and_increment(array, i, j)
         swap_arrays(array, i, j)
         i + 1
@@ -41,6 +50,49 @@ module Course1
 
       def swap_arrays(array, i, j)
         array[j], array[i] = array[i], array[j]
+      end
+
+      def select_pivot_and_i(array, left, right)
+        case @manner
+          when :first
+            pivot, i = array[left], calculate_i(left)
+            return pivot, i
+          when :last
+            right_index = right-1
+            pivot, i = array[right_index], calculate_i(left)
+
+            swap_arrays(array, right_index, left)
+            return pivot, i
+          when :middle
+            left_elemnt, right_element, length = array[left], array[right-1], (right-left)
+            middle = select_middel_element(array, left, length)
+            pivot, i = median(left_elemnt, right_element, middle), calculate_i(left)
+            pivot_index = array.index(pivot)
+
+            swap_arrays(array, pivot_index, left)
+            return pivot, i
+          when :random
+            pivot_index = rand(left...right)
+            pivot, i = array[pivot_index], calculate_i(left)
+
+            swap_arrays(array, pivot_index, left)
+            return pivot, i
+          else
+            raise NotImplementedError
+        end
+      end
+
+      def calculate_i(left)
+        left+1
+      end
+
+      def select_middel_element(array, left, length)
+        length % 2 == 0 ? middle = array[left + length/2 - 1] : middle = array[left + length/2]
+        middle
+      end
+
+      def median(a, b, c)
+        [a, b, c].sort[1]
       end
     end
   end
--- coursera_algorithms/spec/course_1/week_3/quick_sort_spec.rb
+++ coursera_algorithms/spec/course_1/week_3/quick_sort_spec.rb
@@ -7,16 +7,60 @@ RSpec.describe Course1::Week3::QuickSort do
     expect(described_class.new([]).sort!).to be_empty
   end
 
-  (0..100).to_a.map do |range|
-    (0..range).to_a.shuffle
-  end.each do |array|
-    it "for #{array} it sorts its properly" do
-      expect(described_class.new(array).sort!).to eq array.sort
+  shared_examples 'is sorting array of uniq elements' do
+    (1..100).to_a.map do |range|
+      (1..range).to_a.shuffle
+    end.each do |array|
+      it "for #{array}" do
+        expect(described_class.new(array, manner).sort!).to eq array.sort
+      end
     end
+  end
+
+  shared_examples 'is sorting array of random element' do
+    (1..100).to_a.map do |range|
+      (1..range).to_a.map { |el| rand(el) }.shuffle
+    end.each do |array|
+      it "for #{array}" do
+        expect(described_class.new(array, manner).sort!).to eq array.sort
+      end
+    end
+  end
+
+  context 'select pivot as first element' do
+    let(:manner) { :first }
+
+    it_behaves_like 'is sorting array of uniq elements'
+    it_behaves_like 'is sorting array of random element'
+  end
+
+  context 'select pivot as last element' do
+    let(:manner) { :last }
+
+    it_behaves_like 'is sorting array of uniq elements'
+    it_behaves_like 'is sorting array of random element'
+  end
+
+  context 'select pivot as medium element' do
+    let(:manner) { :middle }
+
+    it_behaves_like 'is sorting array of uniq elements'
+  end
+
+  context 'select pivot as random element' do
+    let(:manner) { :random }
+
+    it_behaves_like 'is sorting array of uniq elements'
+    it_behaves_like 'is sorting array of random element'
   end
 
   it "is not creating another array" do
     array = [1, 2, 3]
     expect(described_class.new(array).sort!).to equal array
+  end
+
+  it "it raises error when wrong manner is given" do
+    array = [1, 2, 3]
+    expect { described_class.new(array, :wrong_manner).sort! }.to raise_error(NotImplementedError)
   end
 end
